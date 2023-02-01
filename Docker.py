e466878a8ed2ff2b8f23d140db78e6e79337f240
Henning Klatt <henning.klatt@uni-ulm.de>
2023-02-01 16:21:15 +0100

    testing docker start

--- Docker.py
+++ Docker.py
@@ -1,8 +1,6 @@
-import subprocess, os
 import docker
 
 from PySide6 import QtCore
-
 from PySide6.QtWidgets import (
     QVBoxLayout,
     QLabel,
@@ -13,11 +11,12 @@ from PySide6.QtWidgets import (
 )
 
 from setup_logger import logging
-
+import time
 
 class DockerTab(QWidget):
     def __init__(self, parent: QWidget):
         super().__init__(parent)
+        #super(ViewController, self).__init__()
 
         self.client = docker.DockerClient(base_url='unix://var/run/docker.sock')
 
@@ -28,16 +27,19 @@ class DockerTab(QWidget):
 
         Gridlayout = QGridLayout()
 
+        button_start_ade = QPushButton("Start ADE")
+        button_start_ade.clicked.connect(self.start_ade)
+        button_start_ade.setFixedSize(btn_size, btn_size)
+
         button_start_ros = QPushButton("Start ROS")
         button_start_ros.clicked.connect(self.start_ros)
         button_start_ros.setFixedSize(btn_size, btn_size)
 
-        layout = QVBoxLayout()
-        layout.setAlignment(QtCore.Qt.AlignCenter)
-        version = QLabel(f"Docker Version: {self.getDockerVersion()}")
-        version.setAlignment(QtCore.Qt.AlignCenter)
-        layout.addWidget(version)
-        layout.addWidget(self.getDockerImages())
+        #layout = QVBoxLayout()
+        #layout.setAlignment(QtCore.Qt.AlignCenter)
+        #version = QLabel(f"Docker Version: {self.getDockerVersion()}")
+        #version.setAlignment(QtCore.Qt.AlignCenter)
+        #layout.addWidget(version)
 
         self.terminal = QPlainTextEdit()
         self.terminal.setReadOnly(True)
@@ -48,50 +50,84 @@ class DockerTab(QWidget):
         self.outputpipe.readyReadStandardError.connect(self.handle_stderr)
         self.outputpipe.start("tail", ["-f", "/mnt/outputpipe"])
 
-        Gridlayout.addWidget(button_start_ros, 0, 0)
-        Gridlayout.addLayout(layout, 0, 1)
+
+        self.dockerRunning = QWidget()
+        self.dockerRunning.setUpdatesEnabled(True)
+
+        Gridlayout.addWidget(button_start_ade, 0, 0)
+        Gridlayout.addWidget(button_start_ros, 1, 0)
+        #Gridlayout.addLayout(layout, 0, 1)
+        Gridlayout.addWidget(self.dockerRunning, 0, 1)
         Gridlayout.addWidget(self.terminal, 1, 1)
 
         self.setLayout(Gridlayout)
 
-    def getDockerImages(self):
-        widget = QWidget()
-        layout = QGridLayout()
-        list = self.client.images.list()
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(500)
+        self.timer.timeout.connect(self.timer_test)
+        self.timer.start()
 
-        label = QLabel("Index")
-        label.setAlignment(QtCore.Qt.AlignRight)
-        layout.addWidget(label, 0, 0)
-        label = QLabel("Image Tag")
-        label.setAlignment(QtCore.Qt.AlignLeft)
-        layout.addWidget(label, 0, 1)
 
-        for index, image in enumerate(list):
-            if (len(image.tags)) > 0:
-                label = QLabel(f"{image.tags[0]}")
-                label.setAlignment(QtCore.Qt.AlignLeft)
-                indexLabel = QLabel(str(index))
-                indexLabel.setAlignment(QtCore.Qt.AlignRight)
-                layout.addWidget(indexLabel, index + 1, 0)
-                layout.addWidget(label, index + 1, 1)
+    def timer_test(self):
+        logging.info("Timer!")
+        self.getDockerRunning()
+        self.DockerLayout.update()
+        self.test2.setText(str(time.time()))
 
-        widget.setLayout(layout)
 
-        return widget
+    def getDockerRunning(self):
+        self.DockerLayout = QGridLayout()
+
+        self.test2 = QLabel("Test")
+        self.DockerLayout.addWidget(self.test2, 0, 3)
+
+        for index, entry in enumerate(["ID", "name", "status"]):
+            label = QLabel(entry)
+            #label.setAlignment(QtCore.Qt.AlignRight)
+            self.DockerLayout.addWidget(label, 0, index)
+
+        for index, container in enumerate(self.client.containers.list()):
+            container_id = self.client.containers.get(container.id)
+            label_name = QLabel()
+            label_name.setText(f"{container_id.name}")
+            logging.info(container_id.name)
+            #label_name.setAlignment(QtCore.Qt.AlignLeft)
+            label_id = QLabel()
+            label_id.setText(f"{container_id.short_id}")
+            #label_id.setAlignment(QtCore.Qt.AlignRight)
+            label_status = QLabel()
+            label_status.setText(f"{container_id.status}")
+            self.DockerLayout.addWidget(label_id, index + 1, 0)
+            self.DockerLayout.addWidget(label_name, index + 1, 1)
+            self.DockerLayout.addWidget(label_status, index + 1, 2)
+
+        self.dockerRunning.setLayout(self.DockerLayout)
+
+        logging.info("docker running")
 
     def getDockerVersion(self):
         version = self.client.version()['Components'][0]['Version']
         logging.info(f"Docker Server version: {version}")
         return version
 
+    def start_ade(self):
+        logging.info("Starting ADE (maybe)...")
+        self.terminal.clear()
+        self.inputpipe.startDetached("/bin/bash", ["-c", "echo 'cd ~/ade-home/2021 && ade start' > /mnt/inputpipe"])
+        #self.update()
+
+
     def start_ros(self):
         logging.info("Starting ROS (maybe)...")
-        self.terminal.clear()
-        # ~/ade-home/2021$ ade start
-        # ~/ade-home/2021$ ade enter
-        self.inputpipe.startDetached("/bin/bash", ["-c", "echo 'cd ~/ade-home/2021' > /mnt/inputpipe"])
-        self.inputpipe.startDetached("/bin/bash", ["-c", "echo 'ade start' > /mnt/inputpipe"])
-
+        for container in self.client.containers.list():
+            ade_container = self.client.containers.get(container.id)
+            if(ade_container.name == "ade"):
+                logging.info(f"Found Container {ade_container.name} with id {container.id}")
+                launch_file = "freedrive_11_combined_perception.launch.py"
+                _, stream = ade_container.exec_run(cmd=f"ros2 launch teamspatzenhirn_launch {launch_file}", stream=True, workdir="/home/spatz/2021")
+                for data in stream:
+                    logging.info(data.decode())
+                    self.terminal.appendPlainText(data.decode())
 
     def handle_stdout(self):
         data = self.outputpipe.readAllStandardOutput()
